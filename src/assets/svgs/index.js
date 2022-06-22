1efa4d78fbcabdb63b909e3f7ea078df4602b6c6
Mrshinezee <ijeomaharinze@gmail.com>
2022-06-22 23:40:05 +0100

    implementing the header

---
+++ src/assets/svgs/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable global-require */
+import { ReactComponent as REPORT_EMPTY_STATE } from "../svgs/empty_state.svg";
+import { ReactComponent as LOGO } from "../svgs/logo.svg";
+import { ReactComponent as LOGOUT } from "../svgs/logout.svg";
+import { ReactComponent as MENU } from "../svgs/menu.svg";
+import { ReactComponent as MONITOR } from "../svgs/monitor.svg";
+import { ReactComponent as PIE } from "../svgs/pie.svg";
+import { ReactComponent as GRAPH } from "../svgs/graph.svg";
+import { ReactComponent as CROSS } from "../svgs/cross.svg";
+import { ReactComponent as ARROW_DOWN } from "../svgs/arrow_down.svg";
+
+const svgs = {
+  LOGO,
+  REPORT_EMPTY_STATE,
+  LOGOUT,
+  MENU,
+  MONITOR,
+  PIE,
+  GRAPH,
+  CROSS,
+  ARROW_DOWN
+};
+
+export { svgs };

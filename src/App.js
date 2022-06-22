e1ba5ccb3756566b9d50b5a3cbf767c5d5bf86e3
Mrshinezee <ijeomaharinze@gmail.com>
2022-06-22 23:14:04 +0100

    structure the pplication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Report</div>;
+  return (
+    <div className="App font-sans">
+      <div className="App-container">
+        <div className="header" />
+        <div className="App-body">
+          <div className="sideNav" />
+          <div className="content" />
+          <div className="sideNav" />
+        </div>
+        <div className="footer start">
+          <div className="l-blue">Terms&Conditions | Privacy policy</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
